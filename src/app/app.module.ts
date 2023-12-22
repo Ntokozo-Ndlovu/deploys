15bccf6c7025031f7d563073be8dfbb552d7ca60
Ntokozo Ndlovu <ntokozometo@gmail.com>
2023-12-22 07:50:36 +0200

    delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,7 +57,7 @@ export function playerFactory() {
 
 @NgModule({
     declarations: [
-        AppComponent,
+        AppComponentG,
         LandingComponent,
         TransactionalRisksComponent,
         SubscribeComponent,
